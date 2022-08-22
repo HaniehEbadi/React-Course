6b009b2ec1bcd50db1a5f9737c59e209ed97765c
HaniehEbadi <haniehebadi@gmail.com>
2022-08-22 23:21:47 +0430

    lifting state up

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
-import {useState } from "react";
 import ProductList from "./Components/Product/ProductList";
+import NavBar from "./Components/NavBar/NavBar";
 import "./style.css"
+import React, {Component} from "react";
 
-// class App extends Component {
-//   state = {
-//     products: [
-//       { title: "React.js", price: "99 $"},
-//       { title: "JavaScript", price: "79 $"}
-//     ]
-//   }
+class App extends Component {
+  state = {
+    products: [
+      { title: "React.js", price: "99 $", id:1, quantity:1},
+      { title: "JavaScript", price: "79 $", id:2, quantity:2},
+      { title: "VSCode", price: "58 $", id:3, quantity:4}
+    ]
+  };
 
-//   clickHandler = () => {
-//     this.setState({
-//       products: [
-//         { title: "React.js", price: "89 $"},
-//         { title: "JavaScript", price: "69 $"}
-//       ]
-//     })
-//   }
+  incrementHandler = (id) => {
+    const products = [...this.state.products]
+    const selectedProduct = this.state.products.find((p) => p.id === id)
+    selectedProduct.quantity += 1
+    this.setState({products : products})
+  }
+  
+  decrementHandler = (id) => {
+    const products = [...this.state.products]
+    const selectedProduct = products.find((p) => p.id === id)
+    if (selectedProduct.quantity === 1) {
+      const filteredProducts = products.filter((p) => p.id !== id);
+      this.setState({products: filteredProducts});
+    } else {
+      selectedProduct.quantity -= 1
+      this.setState({products : products})
+    }
+  }
 
-//   render() {
-//     return(
-//       <div className="container" id="title"> 
-//         <h1>shopping App</h1>
-//         {this.state.products.map((product) => {
-//           return <Product name={product.title} price={product.price} />;
-//         })}
-//         <button onClick={this.clickHandler}>change price</button>
-//       </div>
-//     )
-//   }
-// }
+  removeHandler = (id) => {
+    const filteredProducts = this.state.products.filter((p) => p.id !== id);
+    this.setState({products: filteredProducts});
+  };
 
-const App = () => {
-  return(
-    <div className="container" id="title"> 
-      <h1>shopping App</h1>
-      <ProductList />
-    </div>
-  );
+  changeHandler = (event,id) => {
+    const products = [...this.state.products]
+    const selectedProduct = this.state.products.find((p) => p.id === id)
+    selectedProduct.title = event.target.value
+    this.setState({products})
+  };
+
+  render() {
+    return(
+      <div className="container" id="title">
+        <NavBar totalItems = {this.state.products.filter((p) => p.quantity > 0).length}/>
+        <ProductList 
+          products={this.state.products}
+          onIncrement={this.incrementHandler}
+          onDecrement={this.decrementHandler}
+          onRemove={this.removeHandler}
+          onChange={this.changeHandler}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Product/ProductList.js
+++ src/Components/Product/ProductList.js
@@ -2,67 +2,30 @@ import React, { Component } from 'react';
 import Product from './Product';
 
 class ProductList extends Component {
-  state = {
-    products: [
-      { title: "React.js", price: "99 $", id:1, quantity:1},
-      { title: "JavaScript", price: "79 $", id:2, quantity:2},
-      { title: "VSCode", price: "58 $", id:3, quantity:4}
-    ]
-  };
-
-  incrementHandler = (id) => {
-    const products = [...this.state.products]
-    const selectedProduct = this.state.products.find((p) => p.id === id)
-    selectedProduct.quantity += 1
-    this.setState({products : products})
-  }
-  
-  decrementHandler = (id) => {
-    const products = [...this.state.products]
-    const selectedProduct = products.find((p) => p.id === id)
-    if (selectedProduct.quantity === 1) {
-      const filteredProducts = products.filter((p) => p.id !== id);
-      this.setState({products: filteredProducts});
-    } else {
-      selectedProduct.quantity -= 1
-      this.setState({products : products})
-    }
-  }
-
-  removeHandler = (id) => {
-    const filteredProducts = this.state.products.filter((p) => p.id !== id);
-    this.setState({products: filteredProducts});
-  };
-
-  changeHandler = (event,id) => {
-    const products = [...this.state.products]
-    const selectedProduct = this.state.products.find((p) => p.id === id)
-    selectedProduct.title = event.target.value
-    this.setState({products})
-  };
-
   renderProduct = () => {
-    if (this.state.products.length === 0) 
+    if (this.props.products.length === 0) 
       return <div>there is no product in cart</div>;
 
-    return this.state.products.map((product) => {
+    return this.props.products.map((product) => {
       return (
         <Product 
           product={product}
-          onIncrement={() => this.incrementHandler(product.id)}
-          onDecrement={() => this.decrementHandler(product.id)}
-          onDelete={() => this.removeHandler(product.id)}
-          onChange={(e) => this.changeHandler(e,product.id)}
+          onIncrement={() => this.props.onIncrement(product.id)}
+          onDecrement={() => this.props.onDecrement(product.id)}
+          onDelete={() => this.props.onRemove(product.id)}
+          onChange={(e) => this.props.onChange(e,product.id)}
         />
       );
     });
   };
 
   render() { 
-    return <div>
-      {!this.state.products.length && <div>go to shoppings</div>}
-      {this.renderProduct()}
-      </div>;
+    return (
+      <div>
+        {!this.props.products.length && <div>go to shoppings</div>}
+        {this.renderProduct()}
+      </div>
+    )
   }
 }
  
