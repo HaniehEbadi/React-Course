fe068e2de8cf7f4c69d8249f1cbf549c4964e580
HaniehEbadi <haniehebadi@gmail.com>
2022-08-28 16:45:21 +0430

    empty

--- src/App.js
+++ src/App.js
@@ -24,11 +24,11 @@ class App extends Component {
   decrementHandler = (id) => {
     const index = this.state.products.findIndex((item) => item.id === id)
     const product = {...this.state.products[index]}
-    const products = [...this.state.products]
     if (product.quantity === 1) {
-      const filteredProducts = products.filter((p) => p.id !== id);
+      const filteredProducts = this.state.products.filter((p) => p.id !== id);
       this.setState({products: filteredProducts});
     } else {
+      const products = [...this.state.products]
       product.quantity -= 1
       products[index] = product
       this.setState({products})
